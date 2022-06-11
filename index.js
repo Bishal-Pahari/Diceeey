440e0ca9e4d6564c217b12f353795cc14e17ac49
Bishal Pahari <61013432+Bishal-Pahari@users.noreply.github.com>
2022-06-11 21:17:39 +0545

    fixed bug on header
    Sometimes h1 content not changing ever after reloading the page

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var randomNumber3 = Math.floor(Math.random() * 6);
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🍾 Boyfriend Wins!"
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTM = "GirlFriend Wins! 🍾"
+  document.querySelector("h1").innerHTML = "GirlFriend Wins! 🍾"
 } else {
   document.querySelector("h1").innerHTML = "🍾DRAW🍾"
 }
